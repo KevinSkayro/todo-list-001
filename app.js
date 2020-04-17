1cd217d8a450242437c347ca6ad09f89d5d7b06a
Kevin Skayro <kev1n-1997@outlook.com>
2020-04-17 10:33:28 -0400

    task completed/uncompleted filter added

--- app.js
+++ app.js
@@ -2,47 +2,52 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const item = todoList.children;
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //Functions
 function addTodo(event) {
   //prevent button from reseting the page
   event.preventDefault();
+  //if statement to avoid adding empty tasks
+  if (todoInput.value.length != 0) {
+    //Todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
 
-  //Todo div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
+    //Create Li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    //place the li inside the div
+    todoDiv.appendChild(newTodo);
 
-  //Create Li
-  const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value;
-  newTodo.classList.add("todo-item");
-  //place the li inside the div
-  todoDiv.appendChild(newTodo);
+    //Check mark button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = "<i class='fas fa-check'></i>";
+    completedButton.classList.add("complete-btn");
+    //place the button inside the div
+    todoDiv.appendChild(completedButton);
 
-  //Check mark button
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = "<i class='fas fa-check'></i>";
-  completedButton.classList.add("complete-btn");
-  //place the button inside the div
-  todoDiv.appendChild(completedButton);
+    //trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+    trashButton.classList.add("trash-btn");
+    //place the button inside the div
+    todoDiv.appendChild(trashButton);
 
-  //trash button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = "<i class='fas fa-trash'></i>";
-  trashButton.classList.add("trash-btn");
-  //place the button inside the div
-  todoDiv.appendChild(trashButton);
+    //append the entire div to the list
+    todoList.appendChild(todoDiv);
 
-  //append the entire div to the list
-  todoList.appendChild(todoDiv);
-
-  //reset and delete todo input after submiting
-  todoInput.value = "";
+    //reset and delete todo input after submiting
+    todoInput.value = "";
+  } else {
+    alert("Please define your task before adding it to your list.");
+  }
 }
 
 function deleteCheck(event) {
@@ -58,4 +63,29 @@ function deleteCheck(event) {
     const completedItem = item.parentElement;
     completedItem.classList.toggle("completed");
   }
+}
+
+function filterTodo(event) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "done":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
